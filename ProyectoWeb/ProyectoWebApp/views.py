261e92ecaf2742c8f28b01fa104d243a5c1e6e48
Maro <crismaro1997@gmail.com>
2022-04-23 12:31:13 -0500

    se agregan las views de crear carpetas y validar carpetas

--- ProyectoWeb/ProyectoWebApp/views.py
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from .forms import personaForm
+from django.http import HttpResponse
+import os
 
 # Create your views here.
 def home(request):
@@ -22,3 +24,29 @@ def lista_personas(request):
 def carga_personas(request):
     form=personaForm()
     return render(request, 'ProyectoWebApp/carga_personas.html', {'form': form})
+
+def create_folder(request):
+    return render(request, "create_folders.html")
+
+def folder(request):
+    if request.GET['folder_name']:
+        folder=request.GET['folder_name']
+        route='D:/Projectos_Django/'
+        os.mkdir(route+folder)
+        os.mkdir(route+folder+'/doc1')
+        os.mkdir(route+folder+'/doc2')
+        os.mkdir(route+folder+'/doc3')
+        os.mkdir(route+folder+'/doc4')
+        mensaje='usuario creado'
+    else:
+        mensaje='No has introducido usuario a crear'
+    return HttpResponse(mensaje)
+
+def validation_folder(request):
+    a=os.listdir('D:\Projectos_Django\maicol romero')
+    b=os.listdir('D:\Projectos_Django\maicol romero\doc2')
+
+    if len(b) > 0:
+        print('True')
+    else:
+        print('False')
