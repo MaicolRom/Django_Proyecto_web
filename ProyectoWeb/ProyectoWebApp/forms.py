70f606f1a88b063ffb83479d044de3c6c735ef60
MaicolRom <mromero19998@universidadean.edu.co>
2022-04-29 21:53:41 -0500

    version que si sirve

--- ProyectoWeb/ProyectoWebApp/forms.py
+++ ProyectoWeb/ProyectoWebApp/forms.py
@@ -1,8 +1,13 @@
 from http.cookies import BaseCookie
 from django import forms
 from .models import Persona
+from crispy_forms.helper import FormHelper
 
 class personaForm(forms.ModelForm):
     class Meta:
         model = Persona
         fields = ('nombre', 'cedula', 'archivo')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
--- ProyectoWeb/ProyectoWebApp/models.py
+++ ProyectoWeb/ProyectoWebApp/models.py
@@ -1,9 +1,11 @@
+from multiprocessing.sharedctypes import Value
 from django.db import models
 
 class Persona(models.Model):
     nombre=models.CharField(max_length=100)
-    cedula=models.IntegerField()
-    archivo=models.FileField(upload_to='archivos/documentos/')
+    cedula=models.IntegerField(blank=True )
+    archivo=models.FileField(upload_to='archivos/documentos/', blank=True)
+
 
 
     def __str__(self):
